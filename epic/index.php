c2e9d9bcfd024d67e98c593c51ac1922d5c5e503
zleyba <mtvzach@gmail.com>
2016-04-16 22:41:53 -0600

    added epic and started planning

---
+++ epic/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>NMISM.com Planning Documents</title>
+	</head>
+	<body>
+		<h1>NMISM</h1>
+		<h2>Welcome</h2>
+		<p>Hello World</p>
+	</body>
+</html>
