4460e1ab4b5372de2e1d7414a3eba77f017ec364
zleyba <mtvzach@gmail.com>
2016-04-16 23:33:39 -0600

    Started writing css and getting layout in order, added bootstrap

--- epic/index.php
+++ epic/index.php
@@ -3,10 +3,34 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>NMISM.com Planning Documents</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="css/style.css" />
 	</head>
 	<body>
-		<h1>NMISM</h1>
-		<h2>Welcome</h2>
-		<p>Hello World</p>
+
+		<div class="container">
+			<div class="row">
+				<h1>NMISM</h1>
+				<div class="col-sm-2 past">
+					<h2>Past Events</h2>
+					<p>this is a past event</p>
+				</div>
+				<div class="col-sm-6 news">
+					<h2>Welcome</h2>
+					<p>Hello World</p>
+				</div>
+				<div class="col-sm-4 widgets">
+				<h2>Join The Action</h2>
+				<p>this will be a social media plug in</p>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
