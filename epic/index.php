e4dbb935e2a52d793b93a703fcd97c962d62fe45
zleyba <mtvzach@gmail.com>
2016-04-29 02:33:42 -0600

    mockup phase 2

--- epic/index.php
+++ epic/index.php
@@ -17,26 +17,71 @@
 
 		<div class="container">
 			<div class="row">
-				<h1>NMISM</h1>
-				<div class="col-sm-2">
-					<h2>Past Events</h2>
+				<div class="col-xs-6 col-sm-3">
+				<img class=logo" src="img/logo.svg">
+				</div>
+				<div class="col-xs-6 col-sm-9">
+
+					<nav class="navbar navbar-default">
+						<div class="container-fluid">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+
+							</div>
+
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav">
+									<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+									<li><a href="#">Link</a></li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">Action</a></li>
+											<li><a href="#">Another action</a></li>
+											<li><a href="#">Something else here</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">Separated link</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">One more separated link</a></li>
+										</ul>
+									</li>
+								</ul>
+
+							</div><!-- /.navbar-collapse -->
+						</div><!-- /.container-fluid -->
+					</nav>
+				</div>
+			</div>
+
+				<div id="firstcol" class="col-sm-3">
+					<h2><em>Past Events</em></h2>
 					<div class="past">
 						<p>Yr blue bottle quinoa, freegan occupy tofu chia four loko williamsburg cardigan poutine seitan waistcoat keffiyeh. Tilde yr polaroid, mlkshk street art gastropub man bun readymade asymmetrical. Taxidermy scenester XOXO, swag tumblr readymade photo booth dreamcatcher locavore fingerstache tattooed venmo cornhole. Pickled retro pork belly, salvia mustache tumblr meditation ennui. Messenger bag meh knausgaard direct trade. Blog skateboard mixtape next level fashion axe. Sustainable readymade heirloom before they sold out, jean shorts waistcoat forage small batch.</p>
 
 					</div>
 				</div>
-				<div class="col-sm-6">
-					<h2>Welcome</h2>
+				<div class="col-sm-5">
+					<h2><em>Welcome</em></h2>
 						<div class="news">
 							<p>Yr blue bottle quinoa, freegan occupy tofu chia four loko williamsburg cardigan poutine seitan waistcoat keffiyeh. Tilde yr polaroid, mlkshk street art gastropub man bun readymade asymmetrical. Taxidermy scenester XOXO, swag tumblr readymade photo booth dreamcatcher locavore fingerstache tattooed venmo cornhole. Pickled retro pork belly, salvia mustache tumblr meditation ennui. Messenger bag meh knausgaard direct trade. Blog skateboard mixtape next level fashion axe. Sustainable readymade heirloom before they sold out, jean shorts waistcoat forage small batch.</p>
 						</div>
 					</div>
-				<div class="col-sm-3">
-				<h2>Join The Action</h2>
+				<div class="col-sm-2">
+				<h2><em>Join The Action</em></h2>
 				<div class="widgets">
 					<p>Yr blue bottle quinoa, freegan occupy tofu chia four loko williamsburg cardigan poutine seitan waistcoat keffiyeh. Tilde yr polaroid, mlkshk street art gastropub man bun readymade asymmetrical. Taxidermy scenester XOXO, swag tumblr readymade photo booth dreamcatcher locavore fingerstache tattooed venmo cornhole. Pickled retro pork belly, salvia mustache tumblr meditation ennui. Messenger bag meh knausgaard direct trade. Blog skateboard mixtape next level fashion axe. Sustainable readymade heirloom before they sold out, jean shorts waistcoat forage small batch.</p>
 				</div>
 			</div>
 		</div>
+		<footer>
+			NMISM &copy; 2016
+		</footer>
 	</body>
 </html>
